895cb6b0ff655b851cd4bdf5da09e2f73fc9f6c5
Lars Baumann <102919582+LarsIT@users.noreply.github.com>
2023-05-01 21:48:32 +0200

    implemented bar plot for keyword frequency

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import code_steps.funcs.get_keywords as kw
-import pandas
+import pandas as pd
+import plotly.express as px
+
 
 if __name__ == '__main__':
 
@@ -9,6 +11,7 @@ if __name__ == '__main__':
 
     count: dict = {}
 
+    #count keywords
     for article in keywords:
         for tag in article: 
             count[tag] = 1 if tag not in count else count[tag] + 1 
@@ -16,8 +19,15 @@ if __name__ == '__main__':
     #sort count based on value
     count = dict(sorted(count.items(), key= lambda y: y[1],reverse=True)) 
     
-    #remove all keywords with count less than 3
-
+    #remove all keywords with count less than 50
     count = {key: val for key, val in count.items() if val > 50}
 
-    print(count)
+    #make 2 lists, one with keywords, one with count values
+    keywords = [x for x in count]
+    frequency = [y for x,y in count.items()]
+
+    #bar plot
+    fig = px.bar(y= frequency, x= keywords)
+    fig.show
+
+
