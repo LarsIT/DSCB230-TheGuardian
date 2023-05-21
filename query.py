6079cbd303b205b6b06d0542322444de16b30532
Lars <lars.bmnn@protonmail.com>
2023-05-21 14:22:12 +0200

    initialized query function, will maybe not be implemented

---
+++ query.py
@@ -0,0 +1,3 @@
+# write a function that returns articles based on a filter
+# filter takes in 2 parameters as keywords and a timeframe
+# we want to get articles that have 2 specified keywords in order to understand what happened in that timeframe
